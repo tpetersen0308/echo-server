10ee5d9cdb0a497817aa7bb15806b7681fa087e6
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-15 09:18:02 -0700

    first commit

---
+++ src/test/java/server/ServerTest.java
@@ -0,0 +1,12 @@
+package server;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ServerTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
